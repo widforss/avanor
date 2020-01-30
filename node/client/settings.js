b974f043cdd76e0303379dee97bce01c1b91e0a3
Aron Widforss <aron@antarkt.is>
2020-01-30 02:10:28 +0100

    Mistakenly added local changes to client settings

--- node/client/settings.js
+++ node/client/settings.js
@@ -13,7 +13,7 @@ var SETTINGS = {
     NO_LAYER_STR:   'No satellite data found.',
   },
   NJUNIS: {
-    NJUNIS_HOST: 'http://10.90.90.229:8008',
+    NJUNIS_HOST: 'https://njunis.avanor.se',
     TIMEZONE: 'Europe/Stockholm',
     OBSPERIOD: 6,
     START_DIST: 6,
